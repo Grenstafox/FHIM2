53e8e25fb343e97bb601ca172777f31ab6cbc113
Gren <124205591+Grenstafox@users.noreply.github.com>
2025-05-16 03:13:48 -0600

    limonada.js

--- Js/limonada.js
+++ Js/limonada.js
@@ -2,10 +2,10 @@ const limonadaContainer = document.getElementById('limonada');
 
 const limonadas = [
   { nombre: 'Limonada', precio: '$18', imagen: 'img/limonada.jpg' },
-  { nombre: 'Limonada de coco', precio: '$25', imagen: 'img/limonada_coco.png' },
-  { nombre: 'Raspado de limon', precio: '$20', imagen: 'img/limonada_natural.png' },
-  { nombre: 'Limonada de fresa', precio: '$25', imagen: 'img/limonada_fresa.png' },
-  { nombre: 'Limonada de fresa', precio: '$25', imagen: 'img/limonada_azul.png' }
+  { nombre: 'Limonada de coco', precio: '$25', imagen: '/FHIM2/img/limonada_coco.png' },
+  { nombre: 'Raspado de limon', precio: '$20', imagen: '/FHIM2/img/limonada_natural.png' },
+  { nombre: 'Limonada de fresa', precio: '$25', imagen: '/FHIM/img/limonada_fresa.png' },
+  { nombre: 'Limonada de fresa', precio: '$25', imagen: '/FHIM/img/limonada_azul.png' }
 ];
 
 limonadas.forEach(limonada => {
